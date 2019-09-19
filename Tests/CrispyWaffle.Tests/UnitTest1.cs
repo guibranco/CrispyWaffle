498ea51fd830381127cbcc2cf2bfb2abab02e309
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2019-09-19 15:35:44 -0300

    Add test and fix appveyor.yml

---
+++ Tests/CrispyWaffle.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+namespace CrispyWaffle.Tests
+{
+    using Xunit;
+
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
