7d5863bf778e5627fc6c6f54bde25cda33b60ad0
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2023-10-25 23:42:38 +0100

    Update ConversionExtensions.cs

--- Src/CrispyWaffle/Extensions/ConversionExtensions.cs
+++ Src/CrispyWaffle/Extensions/ConversionExtensions.cs
@@ -52,7 +52,8 @@ namespace CrispyWaffle.Extensions
         /// <returns>The given data converted to a Boolean.</returns>
         public static bool ToBoolean(this string str, string validValueForTrue = "S")
         {
-            return str?.Equals(validValueForTrue, StringComparison.InvariantCultureIgnoreCase) == true;
+            return str?.Equals(validValueForTrue, StringComparison.InvariantCultureIgnoreCase)
+                == true;
         }
 
         /// <summary>
