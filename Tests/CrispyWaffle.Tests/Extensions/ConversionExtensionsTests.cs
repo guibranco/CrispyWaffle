9c13a34e485afc26abf629315f4d1bbc9427f62f
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-01-26 23:46:51 +0000

    Fix tests

--- Tests/CrispyWaffle.Tests/Extensions/ConversionExtensionsTests.cs
+++ Tests/CrispyWaffle.Tests/Extensions/ConversionExtensionsTests.cs
@@ -1,15 +1,12 @@
-﻿// ***********************************************************************
-// Assembly         : CrispyWaffle.Tests
-// Author           : Guilherme Branco Stracini
-// Created          : 07-29-2020
+﻿// *********************************************************************** Assembly :
+// CrispyWaffle.Tests Author : Guilherme Branco Stracini Created : 07-29-2020
 //
-// Last Modified By : Guilherme Branco Stracini
-// Last Modified On : 21/03/2023
-// ***********************************************************************
+// Last Modified By : Guilherme Branco Stracini Last Modified On : 21/03/2023 ***********************************************************************
 // <copyright file="ConversionExtensionsTests.cs" company="Guilherme Branco Stracini ME">
 //     Copyright (c) Guilherme Branco Stracini ME. All rights reserved.
 // </copyright>
-// <summary></summary>
+// <summary>
+// </summary>
 // ***********************************************************************
 
 using System;
@@ -153,7 +150,7 @@ public class ConversionExtensionsTests
         );
 
         Assert.Equal(
-            $"The value '{cleanPhoneNumber}' isn't a valid telephone number",
+            $"The value '{cleanPhoneNumber}' isn't a valid telephone number.",
             result.Message
         );
     }
