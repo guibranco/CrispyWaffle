4f09fd558707709f6e2372aa0d6f5f3ca4e2e686
Simran Gill <111714647+SimranGil@users.noreply.github.com>
2024-04-19 01:08:23 +1000

    Moved EventLog files to separate project (#369)
    * Moved EventLog files to separate project
    
    * Updated the project name in soln file
    
    * Rename CrispyWaffle.EventLog.csproj to CrispyWaffle.Eventlog.csproj
    
    Updated the csproj file
    
    * Updated the folder name

---
+++ Src/CrispyWaffle.Eventlog/Log/Providers/EventIdProvider.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace CrispyWaffle.Eventlog.Log.Providers
+{
+    /// <summary>
+    /// Class EventIdProvider.
+    /// Implements the <see cref="CrispyWaffle.Log.Providers.IEventIdProvider" />
+    /// </summary>
+    /// <seealso cref="CrispyWaffle.Log.Providers.IEventIdProvider" />
+    public class EventIdProvider : IEventIdProvider
+    {
+        /// <summary>
+        /// Compute a 32-bit hash of the provided <paramref name="message"/>. The
+        /// resulting hash value can be uses as an event id in lieu of transmitting the
+        /// full template string.
+        /// </summary>
+        /// <param name="message">A message template.</param>
+        /// <returns>A 32-bit hash of the template.</returns>
+        static int Compute(string message)
+        {
+            if (message == null)
+            {
+                throw new ArgumentNullException(nameof(message));
+            }
+            unchecked
+            {
+                uint hash = 0;
+
+                foreach (var t in message)
+                {
+                    hash += t;
+                    hash += (hash << 10);
+                    hash ^= (hash >> 6);
+                }
+
+                hash += (hash << 3);
+                hash ^= (hash >> 11);
+                hash += (hash << 15);
+
+                return (ushort)hash;
+            }
+        }
+
+        /// <summary>
+        /// Computes the event identifier.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        /// <returns>System.UInt16.</returns>
+        public ushort ComputeEventId(string message) => (ushort)Compute(message);
+    }
+}
