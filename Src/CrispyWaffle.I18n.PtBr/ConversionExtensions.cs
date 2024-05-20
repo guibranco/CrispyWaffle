8f2bf589c72a9cf53fa6680699893143f62f33f7
Guilherme Branco Stracini <guilhermes@outsurance.ie>
2024-05-20 21:58:29 +0100

    Fix using

--- Src/CrispyWaffle.I18n.PtBr/ConversionExtensions.cs
+++ Src/CrispyWaffle.I18n.PtBr/ConversionExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.RegularExpressions;
+﻿using System;
+using System.Text.RegularExpressions;
 using CrispyWaffle.Extensions;
 using CrispyWaffle.GoodPractices;
 using CrispyWaffle.Utilities;
