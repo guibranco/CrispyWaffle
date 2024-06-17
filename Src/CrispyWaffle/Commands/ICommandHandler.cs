ebf587bfb89c3b249f3962ce43bc5cbe6fcf62ac
Thiago Christopher <thiago_cfc@hotmail.com>
2024-06-17 16:25:51 -0300

    feat(Commands): Adding commands

---
+++ Src/CrispyWaffle/Commands/ICommandHandler.cs
@@ -0,0 +1,17 @@
+﻿namespace CrispyWaffle.Commands;
+
+/// <summary>
+/// The command handler interface.
+/// </summary>
+/// <typeparam name="TCommand">The type of the command.</typeparam>
+/// <typeparam name="TResult">The type of command result.</typeparam>
+public interface ICommandHandler<in TCommand, out TResult>
+where TCommand : ICommand
+{
+    /// <summary>
+    /// Handles the specified command.
+    /// </summary>
+    /// <param name="command">The command.</param>
+    /// <returns>Result of the handled command.</returns>
+    TResult Handle(TCommand command);
+}
