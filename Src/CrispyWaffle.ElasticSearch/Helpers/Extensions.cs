84ef0de93adf0b5c76a27d3cb305d69bbbb97bc4
Muhammad Haris <Agha.Mohammad.Haris@gmail.com>
2024-07-28 13:24:45 +0500

    Update ElasticSearch package to version 8+ for ElasticSearch project.

--- Src/CrispyWaffle.ElasticSearch/Helpers/Extensions.cs
+++ Src/CrispyWaffle.ElasticSearch/Helpers/Extensions.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Linq.Expressions;
+using System.Threading.Tasks;
 using CrispyWaffle.Composition;
 using CrispyWaffle.ElasticSearch.Utils.Communications;
-using Nest;
+using Elastic.Clients.Elasticsearch;
+using Elastic.Clients.Elasticsearch.IndexManagement;
 
 namespace CrispyWaffle.ElasticSearch.Helpers
 {
@@ -31,7 +33,7 @@ namespace CrispyWaffle.ElasticSearch.Helpers
                     is IndexNameAttribute[] attributes
                 && attributes.Length == 1
                 ? attributes[0].IndexName
-                : type.Name.ToLower().Replace(@" ", @"-");
+                : type.Name.ToLowerInvariant().Replace(@" ", @"-");
         }
 
         /// <summary>
@@ -42,7 +44,7 @@ namespace CrispyWaffle.ElasticSearch.Helpers
         /// <param name="alias">The alias.</param>
         /// <param name="indexName">Name of the index.</param>
         /// <returns>T.</returns>
-        public static T Alias<T>(this T index, string alias, string indexName = null)
+        public static async Task<T> AliasAsync<T>(this T index, string alias, string indexName = null)
             where T : class, IIndexable, new()
         {
             if (string.IsNullOrWhiteSpace(indexName))
@@ -50,9 +52,14 @@ namespace CrispyWaffle.ElasticSearch.Helpers
                 indexName = GetIndexName<T>();
             }
 
-            _connector.Client.Indices.BulkAlias(a =>
-                a.Add(add => add.Index(indexName).Alias(alias))
+            await _connector.Client.Indices.UpdateAliasesAsync(aliases =>
+                aliases.Actions(
+                    actions => actions.Add(
+                        new AddAction() { Index = indexName, Alias = alias }
+                    )
+                )
             );
+
             return index;
         }
 
@@ -62,49 +69,28 @@ namespace CrispyWaffle.ElasticSearch.Helpers
         /// <typeparam name="T">The type to be indexed.</typeparam>
         /// <param name="index">The index.</param>
         /// <returns>T.</returns>
-        public static T Delete<T>(this T index)
+        public static async Task<T> DeleteAsync<T>(this T index)
             where T : class, IIndexable, new()
         {
             var indexName = GetIndexName<T>();
-            if (_connector.Client.Indices.Exists(indexName).Exists)
+            if ((await _connector.Client.Indices.ExistsAsync(indexName)).Exists)
             {
-                _connector.Client.Indices.Delete(indexName);
+                await _connector.Client.Indices.DeleteAsync(indexName);
             }
 
             return index;
         }
 
         /// <summary>
-        /// Automatics the map.
-        /// </summary>
-        /// <typeparam name="T">The type to be indexed.</typeparam>
-        /// <param name="index">The index.</param>
-        /// <returns>T.</returns>
-        public static T AutoMap<T>(this T index)
-            where T : class, IIndexable, new()
-        {
-            var indexName = GetIndexName<T>();
-            if (!_connector.Client.Indices.Exists(indexName).Exists)
-            {
-                _connector.Client.Indices.Create(
-                    indexName,
-                    descriptor => descriptor.Map(ms => ms.AutoMap<T>())
-                );
-            }
-
-            return index;
-        }
-
-        /// <summary>
-        /// Deletes the by query.
+        /// Deletes the document by query.
         /// </summary>
         /// <typeparam name="T">The type to be indexed.</typeparam>
         /// <param name="index">The index.</param>
         /// <param name="field">The field.</param>
         /// <param name="indexPattern">The index pattern.</param>
         /// <param name="daysBefore">The days before.</param>
-        /// <returns>System.Int64.</returns>
-        public static long DeleteByQuery<T>(
+        /// <returns>A task to be awaited on.</returns>
+        public static async Task<long?> DeleteByQueryAsync<T>(
             this T index,
             Expression<Func<T, object>> field,
             string indexPattern,
@@ -112,14 +98,16 @@ namespace CrispyWaffle.ElasticSearch.Helpers
         )
             where T : class, new()
         {
-            var result = _connector.Client.DeleteByQuery<T>(d =>
-                d.Index(indexPattern)
-                    .Query(q =>
-                        q.DateRange(g =>
-                            g.Field(field).LessThan(DateMath.Now.Subtract($@"{daysBefore}d"))
+            var result = await _connector.Client.DeleteByQueryAsync<T>(
+                indexPattern,
+                d => d.Query(
+                    q => q.Range(
+                        r => r.DateRange(
+                            dr => dr.Field(field).Lt(DateMath.Now.Subtract($@"{daysBefore}d"))
+                            )
                         )
                     )
-            );
+                );
 
             return result.Deleted;
         }
